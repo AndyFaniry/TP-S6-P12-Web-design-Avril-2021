215f41c1d58dcb40fb16b498de363990027a29eb
Andy Faniry <ramarosonandyfaniry@gmail.com>
2021-05-07 22:54:29 +0300

    Update Sitemap.java

--- src/main/java/com/infocovid/controller/Sitemap.java
+++ src/main/java/com/infocovid/controller/Sitemap.java
@@ -1,6 +1,11 @@
 package com.infocovid.controller;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
@@ -8,6 +13,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Sitemap {
 	
-	@RequestMapping(value = "sitemap.xml", method = RequestMethod.GET, produces = "application/xml")
+	@RequestMapping(value = "sitemap", method = RequestMethod.GET, produces = "application/xml")
 	public ResponseEntity<InputStreamResource> sitemap() throws IOException {
 		ClassPathResource resource = new ClassPathResource("sitemap.xml");
 	    HttpHeaders headers = new HttpHeaders();
@@ -29,5 +35,16 @@ public class Sitemap {
 	            .contentLength(resource.contentLength())
 	            .contentType(MediaType.parseMediaType("application/octet-stream"))
 	            .body(new InputStreamResource(resource.getInputStream()));
+	}
+	@RequestMapping(value = "/sitemap.xml")
+	public void robots(HttpServletRequest request, HttpServletResponse response) {
+	    try {
+	    	Resource resource = new ClassPathResource("sitemap.xml");
+			Reader reader = new InputStreamReader(resource.getInputStream());
+			String xml=FileCopyUtils.copyToString(reader);
+	        response.getWriter().write(xml);
+	    } catch (IOException e) {
+	      System.out.println(e.getMessage());
+	    }
 	}
 }
